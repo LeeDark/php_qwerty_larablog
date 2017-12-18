598b46ded48cfa97c512491b71201c763aef6457
LeeDark <sergey.povarnin@gmail.com>
2017-12-18 18:31:12 +0200

    Simplify working with timestamps.

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -25,7 +25,7 @@
     <div class="card-footer text-muted">
         <div class="card-text">Posted on {{ $post->created_at->format('l jS \\of F Y h:i:s A') }} by {{ $post->author }}</div>
         
-        @if (Carbon\Carbon::parse($post->updated_at)->ne(Carbon\Carbon::parse($post->created_at)))
+        @if ($post->updated_at->ne($post->created_at))
         <div class="card-text">Updated on {{ $post->updated_at->format('l jS \\of F Y h:i:s A') }}</div>
         @endif
     </div>
